c2a3c3c46d052eb12cf7914001a8cf3e74b768ab
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-23 11:29:21 +0200

    pubsub class and redux connection

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 import appReducer from './reducers';
-import './pubsub';
+import PubSub from './pubsub';
 import './index.css';
 
 const store = createStore(appReducer);
 
 store.subscribe(() => console.log('Store -->', store.getState()));
+
+const pubsub = new PubSub();
+
+pubsub.addListener({
+  message: messageObject => {
+    const { message, channel } = messageObject;
+    console.log('Message Received:', message, ' from channel:', channel);
+    store.dispatch(message);
+  }
+});
+
+setTimeout(() => {
+  pubsub.publish({ type: 'foo', value: 'bar' });
+}, 1000)
 
 ReactDOM.render(
   <Provider store={store}>
--- src/pubsub.js
+++ src/pubsub.js
@@ -1,21 +1,23 @@
 import PubNub from 'pubnub';
 import pubnubConfig from './pubnub.config';
 
-const pubnub = new PubNub(pubnubConfig);
+
 
 export const MESSAGE_CHANNEL = 'MESSAGE_CHANNEL';
 
-pubnub.subscribe({ channels: [MESSAGE_CHANNEL] });
+class PubSub {
+  constructor() {
+    this.pubnub = new PubNub(pubnubConfig);    
+    this.pubnub.subscribe({ channels: [MESSAGE_CHANNEL] });
+  }
 
-pubnub.addListener({
-  message: messageObject => {
-    console.log('PN:MO:', messageObject);
-  } 
-});
+  addListener = listenerConfig => {
+    this.pubnub.addListener(listenerConfig);
+  }
 
-setTimeout(() => {
-  pubnub.publish({
-    message: 'foo',
-    channel: MESSAGE_CHANNEL
-  });
-}, 1000);
+  publish = message => {
+    this.pubnub.publish({ message: message, channel: MESSAGE_CHANNEL });
+  }
+}
+
+export default PubSub;
